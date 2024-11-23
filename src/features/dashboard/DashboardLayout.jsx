f9d8d4fdc08705c382b35795906455631802b8ee
AnetaBishchuk <anna.bishchuk@lnu.edu.ua>
2024-11-23 02:25:42 +0200

    Create a dashboard page with filters

---
+++ src/features/dashboard/DashboardLayout.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { useRecentStays } from "./useRecentStays";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import { useNest } from "../nests/useNest";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
+import GuestsChart from "./GuestsChart";
+
+const StyledDashboardLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+  gap: 2.4rem;
+`;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
+  const { nests, isLoading: isLoading3 } = useNest();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        nestCount={nests.length}
+      />
+      <GuestsChart confirmedStays={confirmedStays} />
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
